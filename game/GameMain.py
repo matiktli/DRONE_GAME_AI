5b11c8102f16350871b293048c9195a40175b799
matiktli <m.kitlinos@gmail.com>
2020-02-02 13:48:19 +0000

    Saving/Loading data via pickle

--- game/GameMain.py
+++ game/GameMain.py
@@ -1,34 +1,40 @@
 from Environment import Environment
 from Bot import Bot, RandomBot
 from utils.config_utils import *
-from data.DataService import DataCollector
+from data.DataService import DataStore
 import data.DataVisualizer as DV
 import sys
 import random
 
 
 # config_path = config_creation_wizard()
-config_path = sys.argv[1]
-is_print = True
+CONFIG_PATH = sys.argv[1]
+IS_DISPLAY = False
+IS_SAVE = True
+GAME_NAME = random.randint(0, 100)
 
-config = Config(config_path)
+
+config = Config(CONFIG_PATH)
 env = Environment(config)
-data_collector = DataCollector()
-data_visualizer = DV.DataVisualizer(random.randint(0, 100), False, True)
+data_store = DataStore()
+data_visualizer = DV.DataVisualizer(
+    str(GAME_NAME), IS_DISPLAY, IS_SAVE)
 
+
+if IS_DISPLAY:
+    input('To start game press [any] key...')
 
 # Start simulating the game
-input('To start game press [any] key...')
 keep_playing = True
 while keep_playing:
     frame = env.get_frame()
-    data_collector.store_game_frame_data(env.engine.cur_turn, frame)
+    data_store.store_game_frame_data(env.engine.cur_turn, frame)
     print('------------------------------------------------')
     for player in env.player_svc.players:
         players_decissions = player.bot.make_decissions(frame)
-        data_collector.store_player_decissions_data(env.engine.cur_turn,
-                                                    player.player_id, players_decissions)
-        if is_print:
+        data_store.store_player_decissions_data(env.engine.cur_turn,
+                                                player.player_id, players_decissions)
+        if IS_DISPLAY:
             print(f"""
             Player making decissions:
                 Player_id: {player.player_id}
@@ -39,9 +45,17 @@ while keep_playing:
 
     keep_playing = env.end_turn()
 
+if IS_SAVE:
+    data_store.save_data_to_file(
+        f'test/FRAME_DATA_{GAME_NAME}.JSON', f'test/DECISION_DATA_{GAME_NAME}.JSON')
+
+    data_store = data_store.with_data_from_file(
+        f'test/FRAME_DATA_{GAME_NAME}.JSON', f'test/DECISION_DATA_{GAME_NAME}.JSON')
+
+
 # After game actions
-data_visualizer.replay_game(data_collector.db_frame,
-                            data_collector.db_decission)
+data_visualizer.replay_game(data_store.db_frame,
+                            data_store.db_decission)
 
 data_visualizer.plot_from_data(
-    data_collector.db_frame, max_turns=config.max_turns, init_players=config.number_of_players)
+    data_store.db_frame, max_turns=config.max_turns, init_players=config.number_of_players)
